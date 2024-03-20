8cf6edbea0cbe6b101bb4e8460f269a6cb726a41
ZZYandZY <18827561151@163.com>
2024-03-20 13:18:14 +0800

    1

---
+++ 2065.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+typedef long long LL;
+
+LL t;
+
+int main()
+{
+    cin>>t;
+    
+    while(t) 
+    {
+        cout<<t<<" ";
+        t/=2;
+    }
+}
