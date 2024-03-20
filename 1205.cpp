54705596a81857170290b7ef3ca47b6121762f72
ZZYandZY <18827561151@163.com>
2024-03-20 12:51:04 +0800

    1

--- 1205.cpp
+++ 1205.cpp
@@ -1,5 +1,5 @@
 //首先猜测一下 打表找规律
-#include<iostream>
+/*#include<iostream>
 
 using namespace std;
 
@@ -27,4 +27,21 @@ int main()
    }
 
    cout<<res;
+}*/
+
+//通过找规律或者裴蜀定理(p,q最大公约数为d。那么一定存在整数a,b,使a*p+b*q = d)
+//如果两个数不互为质数，那么没有最大不能组成的数。（只要这个数不是他们最大公约数的倍数）
+//可以得知 互质的两个数p,q存在不能组成的最大数且为p*q-p-q
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int m,n;
+
+    cin>>m>>n;
+
+    cout<<m*n-m-n;
 }
